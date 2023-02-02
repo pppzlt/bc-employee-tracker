f3226bd6e3e6c3579279b082b4a5b7968323c9f7
Lantao home PC <lantaook@gmail.com>
2023-02-02 00:37:31 -0500

    insert values

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,31 @@
-INSERT INTO department ()
+INSERT INTO
+  department (name)
+VALUES
+  ('FINANCE'),
+  ('SALES'),
+  ('ENGINEERING'),
+  ('LEGAL');
+
+INSERT INTO
+  role (title, salary, department_id)
+VALUES
+  ('Sales Lead', 100000, 2),
+  ('Salesperson', 80000, 2),
+  ('Lead Engineer', 150000, 3),
+  ('Software Engineer', 120000, 3),
+  ('Account Manager', 160000, 1),
+  ('Accountant', 125000, 1),
+  ('Legal Team Lead', 250000, 4),
+  ('Lawyer', 190000, 4);
+
+INSERT INTO
+  employee (first_name, last_name, role_id, manager_id)
+VALUES
+  ('John','Doe',1,NULL),
+  ('Mike','Chan',2,1),
+  ('Ashley','Rodrigues',3,NULL),
+  ('Kevin','Tupik',4,3),
+  ('Kunal','Singh',5,NULL),
+  ('Malia','Brown',6,5),
+  ('Sarah','Lourd',7,NULL),
+  ('Tom','Allen',8,7);
